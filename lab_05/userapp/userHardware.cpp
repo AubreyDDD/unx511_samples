4aa0a8ae9233e6f4d86c8c0fcfa4eabf9af48658
John Sellens <jsellens@syonex.com>
2025-10-07 02:17:02 -0400

    lab 5 5b and 8 renumber with leading 0s

---
+++ lab_05/userapp/userHardware.cpp
@@ -0,0 +1,57 @@
+//userHardware.cpp - user level code for using a hardware device
+//
+// 29-Dec-20  M. Watler         Created.
+//
+#include <fcntl.h>
+#include <stdio.h>
+#include <string.h>
+#include <sys/ioctl.h>
+#include <unistd.h>
+#include "hardwareDevice.h"
+
+const char devFile[]="/dev/hardware";
+const int  BUF_LEN=16;
+
+using namespace std;
+
+int main()
+{
+    int fd, rc=0;
+    char buf[BUF_LEN];
+    int  nbytes;
+
+    fd=open(devFile, O_RDWR);
+    if(fd < 0) {
+        printf("Cannot open the device file...\n");
+        rc=-1;
+    }
+    if(rc==0) {
+        for(int i=0; i<3; ++i) {
+            nbytes=read(fd, buf, BUF_LEN);
+            printf("nbytes:%d = read() buf:%s\n", nbytes, buf);
+            sleep(3);
+// should complain if we get the same thing as before
+        }
+        printf("HARDWARE_DEVICE_HALT...\n");
+        ioctl(fd, HARDWARE_DEVICE_HALT, NULL);
+        for(int i=0; i<3; ++i) {
+// should complain if we don't get the same thing each time
+            nbytes=read(fd, buf, BUF_LEN);
+            printf("nbytes:%d = read() buf:%s\n", nbytes, buf);
+            sleep(3);
+        }
+        printf("HARDWARE_DEVICE_RESUME...\n");
+        ioctl(fd, HARDWARE_DEVICE_RESUME, NULL);
+        for(int i=0; i<3; ++i) {
+            nbytes=read(fd, buf, BUF_LEN);
+            printf("nbytes:%d = read() buf:%s\n", nbytes, buf);
+            sleep(3);
+// should complain if we get the same thing as before
+        }
+    }
+
+    close(fd);
+
+    return rc;
+}
+
---
+++ lab_05b/README.md
@@ -0,0 +1,6 @@
+# unx511_samples for lab 5b
+
+UNX511 sample / example code
+for lab 5b on Signals
+for UNX511 section NSB
+summer 2025.
